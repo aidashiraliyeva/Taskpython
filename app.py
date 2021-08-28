df3a7cb291adeabe4e94947a20c812c375c226c8
aidashiraliyeva <shiraliyevaida1997@gmail.com>
2021-08-28 18:24:33 +0400

    Up

--- app.py
+++ app.py
@@ -1,92 +1,77 @@
-from flask import Flask,render_template,request
-from werkzeug.utils import redirect
+from flask import Flask, render_template, url_for,request,redirect
+from flask_sqlalchemy import SQLAlchemy
+import os
 
-Books = []
+app=Flask(__name__)
+UPLOAD_FOLDER='static/uploads/'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
+app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
+db=SQLAlchemy(app)
 
-app = Flask(__name__)
+
+class Post (db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    details=db.Column(db.String(50))
+
+
+class News(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    news_img=db.Column(db.String(50))
+    news_heading=db.Column(db.String(50))
+    news_date=db.Column(db.String(50))
+
+db.create_all()
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/create_book', methods = ['GET', 'POST'])
-def create_book():
-    if request.method == 'POST':
-        Name = request.form['Name']
-        Author = request.form['Author']
-        Year = request.form['Year']
-        Number = request.form['Number']
-        Summary = request.form['Summary']
-        Book = {
-            'id':Books.__len__(),
-            'Name': Name,
-            'Author': Author,
-            'Year': Year,
-            'Number': Number,
-            'Summary': Summary
-        }
-        Books.append(Book)
-        
-        
-    return render_template('create.html')
-
-
-
-@app.route('/show')
-def show():
-    return render_template('show.html', Books = Books)
-
-@app.route('/single/<id>')
-def single(id):
-    id=int(id)
-    book={}
-
-    for i in Books:
-       if id==i['id']:
-           book=i
-    return render_template('single.html',book=book) 
-    
+def admin_index():
+    news=News.query.all()
+    return render_template('index.html',news =news)
 
 @app.route('/admin')
 def admin():
-    return render_template('admin.html', Books = Books)
+    return render_template('admin.html')
+
+@app.route('/show')
+def show():
+    posts=Post.query.all()
+    return render_template('show.html',posts=posts)
 
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    for book in Books:
-        if id==book['id']:
-           Books.remove(book)
-    return redirect('/admin')
-
-
-@app.route('/update/<int:id>',methods=['POST','GET'])
-def update():
-    data={}
-    for book in Books:
-        if id==book['id']:
-            data=book
-            if request.method=='POST':
-                Name=request.form['Name']
-                Author=request.form['Author']
-                Year=request.form['Year']
-                Number=request.form['Number']
-                Summary=request.form['Summary']
-                Books.remove(book)
-                data={
-                    'id':book['id'],
-                    'Name':Name,
-                    'Author':Author,
-                    'Year':Year,
-                    'Number':Number,
-                    'Summary':Summary
-                }
-                Books.append(data)
-                return redirect('/admin')
-    return render_template('update.html',data=data)
+@app.route('/create', methods=['GET','POST']) 
+def create():
+    
+    if request.method=='POST':
+        file=request.files['news_img']
+        filename=file.filename
+        file.save(os.path.join (app.config['UPLOAD_FOLDER'],filename))
+        new=News(
+        news_img=filename,
+        news_heading=request.form['news_heading'],
+        news_date=request.form['news_date']
+        )
+        db.session.add(new)
+        db.session.commit()
+        return redirect("/")
+    return render_template('create_post.html')
 
 
 
-if __name__=='__main__':
+
+@app.route('/post') 
+def post(): 
+    return render_template('post.html')
+
+@app.route('/post', methods=['GET','POST']) 
+def postadd():
+    if request.method=='POST':
+        post=Post(details=request.form.get('post'))
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('admin_index'))
+
+    return render_template('show.html')
+
+
+
+if __name__ == '__main__':
     app.run(debug=True)
